e0e4647f6bd1bf3150e9b5dbf41a2937e940b3bc
Bran <jiakuo.zhang@outlook.com>
2022-10-17 17:44:15 +0800

    ud

---
+++ ParquetParse/Program.cs
@@ -0,0 +1,16 @@
+﻿while (true)
+{
+    Console.WriteLine("Please wirte the parquet file name: ");
+    var parquetFileName = Console.ReadLine();
+    if (!File.Exists(parquetFileName))
+    {
+        Console.WriteLine($"Not found the file: {parquetFileName}.");
+        return;
+    }
+    var reader = await ParquetReader.ReadTableFromFileAsync(parquetFileName);
+    foreach (var item in reader)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine("--------------------------------------------------------------");
+}
