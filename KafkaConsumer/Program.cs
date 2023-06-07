3a2997f6af1796f81f6b5518613f0739f4658e67
面包牛奶 <88377252+Jiakuo0x@users.noreply.github.com>
2023-06-07 18:25:02 +0800

    set automatic offset

--- KafkaConsumer/Program.cs
+++ KafkaConsumer/Program.cs
@@ -5,7 +5,7 @@ var config = new ConsumerConfig
     BootstrapServers = "124.70.40.236:22102", // Kafka集群的地址
     GroupId = "my_consumer_group",        // 设置groupid
     AutoOffsetReset = AutoOffsetReset.Earliest, // 设置消费者在启动时从哪个偏移量开始消费
-    EnableAutoCommit = false                     // 禁用自动提交偏移量
+    EnableAutoCommit = true                     // 禁用自动提交偏移量
 };
 
 using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
